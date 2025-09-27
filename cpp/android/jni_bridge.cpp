d82a147862e1c0f4948bc28524baa656db86f31b
Kazuma Arino <hogeika2@gmail.com>
2025-09-27 21:56:14 +0900

    Update JNI analyze format.

--- cpp/android/jni_bridge.cpp
+++ cpp/android/jni_bridge.cpp
@@ -556,11 +556,11 @@ struct GTPEngine {
           lcb = 1.0 - lcb;
         }
         sstream << Location::toString(data.move,board);
-        sstream << "," << round(winrate * 10000.0);
+        sstream << " " << round(winrate * 10000.0) << " ";
         if(preventEncore && data.pvContainsPass())
-          data.writePVUpToPhaseEnd(cout,board,search->getRootHist(),search->getRootPla());
+          data.writePVUpToPhaseEnd(sstream,board,search->getRootHist(),search->getRootPla());
         else
-          data.writePV(cout,board);
+          data.writePV(sstream,board);
       }
       sstream << endl;
     };
@@ -1437,6 +1437,13 @@ Java_io_github_karino2_paoogo_goengine_katago_KataGoNative_setBoardSize (
 }
 
 // 固定時間で同期的に振る舞う関数。文字列で結果を返す。
+// lz-analyzeを元にした文字列。
+// [KataGo/docs/GTP_Extensions.md at master · lightvector/KataGo](https://github.com/lightvector/KataGo/blob/master/docs/GTP_Extensions.md)
+// winrate: [0, 10000]
+// 先頭が候補手とwinrate、そのあとにpvが続く。空白区切り。
+// pvが終わったらカンマ区切りで次のinfo。
+// 例としては以下のような出力。
+// F4 5506 F4 E2 G4 E7 F7 H3,D6 5546 D6 D7 C7 E6 D5 E7 E5 C8 B7 B8 B6,D7 5341 D7 D6 E6 C7 D8 B4,G5 5099 G5 D2 D7 C7 C3,C5 5092 C5 D7 F4 H5,E3 4829 E3 H5 H6 G5 G6 D7,E7 4955 E7 D7 F4 E2,E5 4706 E5 B4 H4,B5 4847 B5 E5,G4 4320 G4,F3 4450 F3,E2 4238 E2,C7 4212 C7,H5 4164 H5
 jstring
 Java_io_github_karino2_paoogo_goengine_katago_KataGoNative_analyze (
 	JNIEnv*	env,
@@ -1448,7 +1455,7 @@ Java_io_github_karino2_paoogo_goengine_katago_KataGoNative_analyze (
   Player pla = IsBlackToColor(isBlack);
   GTPEngine::AnalyzeArgs args;
   args.analyzing = true;
-  args.secondsPerReport = 1.0;
+  args.secondsPerReport = (((double)msec)/1000.0) - 0.1;
   args.minMoves = 0;
   args.maxMoves = 10000000;
   args.avoidMoveUntilByLocBlack = vector<int>{};
