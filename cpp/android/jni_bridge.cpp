c3bb0a62157e8268a71ea43f75e40d27d0deb43c
Hiraoka <kakkokakko@gmail.com>
2025-10-24 01:30:26 +0900

    Tentative workaround for slow analysis after board size change

--- cpp/android/jni_bridge.cpp
+++ cpp/android/jni_bridge.cpp
@@ -885,7 +885,9 @@ struct GTPEngine {
   void analyze(Player pla, AnalyzeArgs args) {
     clearStream();
     assert(args.analyzing);
-    if(isGenmoveParams) {
+    // if(isGenmoveParams) {
+    if(true) {  // Tentative workaround for slow analysis after board size change
+      analysisParams.humanSLProfile = SGFMetadata::getProfile("_");  // ditto
       bot->setParams(analysisParams);
       isGenmoveParams = false;
     }
